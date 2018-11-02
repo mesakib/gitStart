e2592a9ad029dc66dd12a5f59d7aff728c07feb1
sakib <shakib.shahariar.00@gmail.com>
2018-11-03 00:18:35 +0600

    fix some bug

--- start.java
+++ start.java
@@ -1,10 +1,10 @@
-public class Start{
+public class start{
 	public static void main(String[] args){
 		System.out.println("Bismillah");	
-		jikir(Alhamdulilah);	
+		jikir("Alhamdulilah");	
 	}
 	public static void jikir(String tasbhi){
 		for(int i = 0; i < 33; i++)
-				System.out.println(i + " " + tasbhi);		
+				System.out.println(i+1 + " " + tasbhi);		
 	}		
 }
