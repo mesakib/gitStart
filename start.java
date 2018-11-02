5dd31a7f50f5b8c8712214db3e8b9a46bb37f8a6
sakib <shakib.shahariar.00@gmail.com>
2018-11-03 00:07:12 +0600

    add new methode called jikir && and put a for loop in it

--- start.java
+++ start.java
@@ -1,5 +1,10 @@
 public class Start{
 	public static void main(String[] args){
-		System.out.println("Bismillah");		
+		System.out.println("Bismillah");	
+		jikir(Alhamdulilah);	
+	}
+	public static void jikir(String tasbhi){
+		for(int i = 0; i < 33; i++)
+				System.out.println(i + " " + tasbhi);		
 	}		
 }
